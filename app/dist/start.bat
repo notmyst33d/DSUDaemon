4f8d4dadcee52a7231a74def641bc0f52c3b7047
Myst33d <notmyst33d@gmail.com>
2021-07-17 22:21:43 +0600

    Runs on my what?

--- app/dist/start.bat
+++ app/dist/start.bat
@@ -1,6 +1,6 @@
 @echo off
 adb push classes.dex /data/local/tmp
-echo DSUDaemon is now running in the background on your, you can close the window now.
+echo DSUDaemon is now running in the background on your phone, you can close the window now.
 echo PS. If you want to kill the daemon do this: adb shell "kill $(cat /data/local/tmp/dsu_pid)"
 echo PPS. Launching start.bat again will also kill the daemon
 adb shell "nohup app_process -Djava.class.path=/data/local/tmp/classes.dex /system/bin dsudaemon.Main &"
